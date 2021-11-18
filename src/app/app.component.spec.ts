5a5d98af13961997ea8f71f429f46b8d27eb6f31
Razei <razeixyz@gmail.com>
2021-11-17 22:00:47 -0500

    Added mocks for AppComponent spec

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -2,6 +2,8 @@ import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 
 import { AppComponent } from './app.component';
+import { DiplomasRepositoryService } from './data/diplomas-repository/diplomas-repository.service';
+import { StudentRepositoryService } from './data/student-repository/student-repository.service';
 import { HasGraduatedPipe } from './pipes/has-graduated.pipe';
 
 describe('AppComponent', () => {
@@ -12,8 +14,12 @@ describe('AppComponent', () => {
       ],
       declarations: [
         AppComponent,
-        HasGraduatedPipe
+        HasGraduatedPipe,
       ],
+      providers: [
+        { provide: StudentRepositoryService, useValue: mockStudentRepositoryService },
+        { provide: DiplomasRepositoryService, useValue: mockDiplomasRepositoryService }
+      ]
     }).compileComponents();
   });
 
@@ -36,3 +42,11 @@ describe('AppComponent', () => {
     expect(compiled.querySelector('h1')?.textContent).toContain('Graduation Tracker app is running!');
   });
 });
+
+const mockStudentRepositoryService = {
+  students: []
+};
+
+const mockDiplomasRepositoryService = {
+  getDiploma: () => {}
+};
